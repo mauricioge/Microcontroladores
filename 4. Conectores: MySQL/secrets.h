f8196b04e705f6ccc48394afc1e02a124623baaf
Mauricio Gonzalez <mauricioge@gmail.com>
2019-09-21 00:48:23 -0600

    Create secrets.h

---
+++ 4. Conectores: MySQL/secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "xxxxxxxx"        // Nombre de red WiFi
+#define SECRET_PASS "xxxxxxxx"        // Contraseña WiFi
+#define SECRET_MYSQL_USER "iot_admin" // Usuario MySQL
+#define SECRET_MYSQL_PASS "12345678"  // Contraseña MySQL
