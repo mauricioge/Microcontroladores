8e6dee54d2036bdc2c36004ae6c7d6634aeaeabd
Mauricio Gonzalez <mauricioge@gmail.com>
2019-09-21 01:11:18 -0600

    Create secrets.h

---
+++ 7. Creando mi servicio IoT/secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "xxxxxxxx" // SSID: Red WiFi a la que se quiere conectar
+#define SECRET_PASS "xxxxxxxx" // Contraseña (WPA)
+#define DHTPIN 5               // Pin que recibe los datos del sensor
+#define DHTTYPE DHT11          // Tipo de sensor: DHT11
