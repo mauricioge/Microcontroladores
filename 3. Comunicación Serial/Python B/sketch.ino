e99805804f8fb745e766174b6aa23a3aff0af08e
Mauricio Gonzalez <mauricioge@gmail.com>
2019-09-21 00:11:40 -0600

    Create sketch.ino

---
+++ 3. Comunicación Serial/Python B/sketch.ino
@@ -0,0 +1,21 @@
+// Autor: mauricioge <mauricioge@gmail.com> 20/Ago/2019
+// Cada 5 segundos envía una línea de datos por medio del puerto serial
+// prg-B.py lee todo lo que la placa envía por el puerto serial
+// Placas: ESP32 | MKR1000 | NodeMCU V1 | ESP-01
+
+String rndTmp;
+String rndHmd;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Ambas variables son alimentadas por valores random
+  rndTmp = random(50);
+  rndHmd = random(80);
+
+  // Las variables son enviadas al puerto serial
+  Serial.println("A4:CF:12:04:CB:1C, " + rndTmp + ", " + rndHmd);
+  delay(5000);
+}
