d0bc6ceda302413ed370a0497f1cf06255fefa4a
Mauricio Gonzalez <mauricioge@gmail.com>
2019-09-23 02:32:33 -0600

    Delete secrets.h

--- 6. Servicios IoT/secrets.h
+++
@@ -1,7 +0,0 @@
-#define SECRET_SSID     "xxxxxxxx" // SSID: Red WiFi a la que se quiere conectar
-#define SECRET_PASS     "xxxxxxxx"  // Contraseña (WPA)
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883 // use 8883 for SSL
-#define AIO_USERNAME    "mauricioge"
-#define AIO_KEY         "69c2780aaa764f7da170f5869f7ab7fa"
-#define LED 6
