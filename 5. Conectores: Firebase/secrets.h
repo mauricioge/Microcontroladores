fd8585792cb805f62dfaeac416ea00e2d4c58cd7
Mauricio Gonzalez <mauricioge@gmail.com>
2019-09-23 02:34:23 -0600

    Create secrets.h

---
+++ 5. Conectores: Firebase/secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "Gonzalez" // SSID: Red WiFi a la que se quiere conectar
+#define SECRET_PASS "12345678" // Contraseña (WPA)
+#define SECRET_FIREBASE_HOST "iot-th.firebaseio.com"                    // Firebase Host
+#define SECRET_FIREBASE_AUTH "hvAt4H8pk5vvVc3mgFLIfw3gOo00Kh87DhKywoX3" // Firebase Secret
